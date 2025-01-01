9a04e5c15d25bc143bb7b6f721ff12ff2bd7ac87
Michal Stehlík <michal.stehlik@pslib.cz>
2025-01-01 11:49:59 +0100

    code prettyfication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Notifications } from "@mantine/notifications";
 import { SessionProvider } from "next-auth/react";
 import { AccountDrawerProvider } from "@/providers/AccountDrawerProvider";
 import { AccountDrawer } from "@/components";
+import Script from "next/script";
 
 import "@mantine/core/styles.css";
 import "@mantine/notifications/styles.css";
@@ -23,6 +24,7 @@ const RootLayout = ({
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
+  const clarityId = process.env.NEXT_PUBLIC_CLARITY_ID;
   return (
     <html lang="cs">
       <head>
@@ -47,6 +49,16 @@ const RootLayout = ({
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ffc0cb" />
         <meta name="msapplication-TileColor" content="#ff1493" />
         <meta name="theme-color" content="#ffffff"></meta>
+        {/* Microsoft Clarity Script */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${clarityId}");
+          `}
+        </Script>
         <ColorSchemeScript />
       </head>
       <body>
